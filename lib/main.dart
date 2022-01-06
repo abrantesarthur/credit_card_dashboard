ff9d9491adfae0c8ce56c9c539083ec84eb4b12f
Arthur Abrantes <abrantesarthur1997@gmail.com>
2022-01-06 00:43:54 -0300

    reestructure app

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,21 @@
-import 'package:credit_card_dashboard/app.dart';
+import 'package:credit_card_dashboard/configure_web.dart';
+import 'package:credit_card_dashboard/screens/transactions.dart';
 import 'package:flutter/material.dart';
 
-void main() async {
-  runApp(App());
+void main() {
+  configureApp();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Venni',
+      initialRoute: Transactions.routeName,
+      routes: {Transactions.routeName: (context) => const Transactions()},
+    );
+  }
 }
