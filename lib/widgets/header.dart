9d8239241380b44a04ad848e004e5827ee89a2b0
Arthur Abrantes <abrantesarthur1997@gmail.com>
2022-01-06 01:12:23 -0300

    add Header widget

--- lib/widgets/header.dart
+++ lib/widgets/header.dart
@@ -1,40 +1,39 @@
 import 'package:flutter/material.dart';
 
 class Header extends StatelessWidget {
+  final String title;
+
+  const Header({
+    Key? key,
+    required this.title,
+  }) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: [
-        InkWell(
-          onTap: () {},
-          child: Image.asset(
-            "images/logo-horizontal-white.png",
-            width: screenWidth /
-                (constraints.maxWidth > 760
-                    ? 6
-                    : constraints.maxWidth >= 501
-                        ? 4
-                        : 3),
+    final screenWidth = MediaQuery.of(context).size.width;
+
+    return LayoutBuilder(builder: (
+      BuildContext context,
+      BoxConstraints constraints,
+    ) {
+      return Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          InkWell(
+            onTap: () {},
+            child: Image.asset(
+              "images/logo-horizontal.png",
+              width: screenWidth /
+                  (constraints.maxWidth > 760
+                      ? 6
+                      : constraints.maxWidth >= 501
+                          ? 4
+                          : 3),
+            ),
           ),
-        ),
-        AppButton(
-          textData: "Trabalhe Conosco",
-          height: constraints.maxWidth > 760 ? 50 : 40,
-          textStyle: TextStyle(
-            color: Colors.white,
-            fontSize: constraints.maxWidth > 760 ? 22 : 18,
-            fontWeight: FontWeight.w600,
-          ),
-          hasShadow: false,
-          border: Border.all(color: Colors.white, width: 2),
-          onTapCallBack: () => Navigator.pushNamedAndRemoveUntil(
-            context,
-            Partners.routeName,
-            (_) => false,
-          ),
-        )
-      ],
-    );
+          Text(title),
+        ],
+      );
+    });
   }
 }
