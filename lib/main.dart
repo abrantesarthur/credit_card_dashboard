246383ad452825204744396831ef1587333cbe76
Arthur Abrantes <abrantesarthur1997@gmail.com>
2022-01-10 04:12:06 -0300

    add provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,29 @@
 import 'package:credit_card_dashboard/configure_web.dart';
+import 'package:credit_card_dashboard/database/interfaces.dart';
+import 'package:credit_card_dashboard/models/creditCard.dart';
 import 'package:credit_card_dashboard/routes/dashboard.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   configureApp();
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
+
+  @override
+  MyAppState createState() => MyAppState();
+}
+
+class MyAppState extends State<MyApp> {
+  final CreditCardModel creditCardModel = CreditCardModel();
+
+  @override
+  void initState() {
+    creditCardModel.downloadData();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
--- lib/models/creditCard.dart
+++ lib/models/creditCard.dart
@@ -1,5 +1,3 @@
-import 'dart:async';
-
 import 'package:credit_card_dashboard/database/interfaces.dart';
 import 'package:credit_card_dashboard/database/methods.dart';
 import 'package:flutter/material.dart';
