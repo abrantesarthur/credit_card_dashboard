85334780580f9475a1c7aeb000b23029fdf7e297
Arthur Abrantes <abrantesarthur1997@gmail.com>
2022-01-10 01:39:00 -0300

    add dummy getTransactions

--- lib/database/methods.dart
+++ lib/database/methods.dart
@@ -1,1 +1,6 @@
+import 'package:credit_card_dashboard/database/data.dart';
+import 'package:credit_card_dashboard/database/interfaces.dart';
 
+Transactions getTransactions() {
+  return Transactions.fromJson(transactions);
+}
